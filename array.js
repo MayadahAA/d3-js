704109c919d2224220df8ba9e0f2554392224892
Mayadah <97852885+MayadahAA@users.noreply.github.com>
2023-08-08 16:26:18 +0300

    commit

--- array.js
+++ array.js
@@ -1,71 +1,71 @@
-// const arr = ["ss", '2', true, 4, 5];
+const arr = ["ss", '2', true, 4, 5];
 
-// // console.log(arr);
-// // console.log(arr.length);
+console.log(arr);
+console.log(arr.length);
 
-// // for (let i = 0; i < arr.length; i++) {
-// //     const element = arr[i];
-// //     console.log(element);
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
     
-// // }
+}
 
 
-// // let color = ['blue', 'black', 'orange'];
+let color = ['blue', 'black', 'orange'];
 
-// // let color2 = ['white', 'red'];
-// // let colors = color.concat(color2);
-// // console.log(colors);
-
-
-// // for (let i = 0; i < numbers.length; i++) {
-// //     if (numbers[i] % 2 == 0) {
-// //         console.log(numbers[i]);
-// //     }
-// // }
+let color2 = ['white', 'red'];
+let colors = color.concat(color2);
+console.log(colors);
 
 
-// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0) {
+        console.log(numbers[i]);
+    }
+}
 
-// let even = numbers.filter(function (currentValue){
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+let even = numbers.filter(function (currentValue){
     
-//     return currentValue % 2 == 0;
-// } 
-// ) 
-// console.log(even);
+    return currentValue % 2 == 0;
+} 
+) 
+console.log(even);
 
-// let odd = numbers.filter(function (currentValue){
-//     return currentValue % 2 != 0;
-// } 
-// ) 
-// console.log(odd);
+let odd = numbers.filter(function (currentValue){
+    return currentValue % 2 != 0;
+} 
+) 
+console.log(odd);
 
-// const firsteven = numbers.find(function (x){
-//     return x % 2 == 0;
-// })
-// console.log(firsteven);
+const firsteven = numbers.find(function (x){
+    return x % 2 == 0;
+})
+console.log(firsteven);
 
-// numbers.forEach(function (x, i){
-//  numbers[i] = x * 2
-// });
-//     console.log(numbers);
-// console.log(numbers.includes(11));
+numbers.forEach(function (x, i){
+ numbers[i] = x * 2
+});
+    console.log(numbers);
+console.log(numbers.includes(11));
 
 
-// let strs = ['drink', 'your', 'coffee', 'now']
+let strs = ['drink', 'your', 'coffee', 'now']
 
-// numbers.sort(function (a, b){
-//     return b - a;
-// });
+numbers.sort(function (a, b){
+    return b - a;
+});
 
-// console.log(numbers);
+console.log(numbers);
 
-// const everyIsOdd = numbers.every(function(x){
-//     return x % 2 != 0;
-// })
-// console.log(everyIsOdd);
+const everyIsOdd = numbers.every(function(x){
+    return x % 2 != 0;
+})
+console.log(everyIsOdd);
 
-// const someIs = numbers.some(function (x){
-//         return x % 2 == 0;
-// }
-// );
-// console.log(someIs);
+const someIs = numbers.some(function (x){
+        return x % 2 == 0;
+}
+);
+console.log(someIs);
